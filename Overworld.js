08d445409107568c962587526faa41ade03cf6cf
christinewang766 <christinewang766@gmail.com>
2023-07-24 22:45:13 -0700

    finished Part 2 #pizzalegends

--- Overworld.js
+++ Overworld.js
@@ -1,55 +1,33 @@
 class Overworld {
- constructor(config) {
-   this.element = config.element;
-   this.canvas = this.element.querySelector(".game-canvas");
-   this.ctx = this.canvas.getContext("2d");
- }
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+  }
 
- init() {
-   const image = new Image();
-   image.onload = () => {
-     this.ctx.drawImage(image,0,0)
-   };
-   image.src = "/images/maps/DemoLower.png";
+  init() {
+    const background = new Image();
+    background.onload = () => {
+      this.ctx.drawImage(background, 0, 0);
+    };
+    background.src = "/images/maps/cherry.png";
 
+    // place Game Objects!
+    const hero = new GameObject({
+      x: 4,
+      y: 12.5,
+    });
 
-   const x = 5;
-   const y = 6;
+    const lishou = new GameObject({
+      x: 15,
+      y: 12.5,
+      src: "/images/characters/people/lishou.png",
+    });
 
-   const shadow = new Image();
-   shadow.onload = () => {
-    this.ctx.drawImage(
-      shadow, 
-      0, //left cut 
-      0, //top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32
-   )
-   }
-   shadow.src = "/images/characters/shadow.png";
-
-
-   const hero = new Image();
-   hero.onload = () => {
-     this.ctx.drawImage(
-       hero, 
-       0, //left cut 
-       0, //top cut,
-       32, //width of cut
-       32, //height of cut
-       x * 16 - 8,
-       y * 16 - 18,
-       32,
-       32
-    )
-   }
-   hero.src = "/images/characters/people/hero.png";
-
-
- }
-
+    // temp !!!!
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx);
+      lishou.sprite.draw(this.ctx);
+    }, 200)
+  }
 }
---
+++ Sprite.js
@@ -0,0 +1,39 @@
+class Sprite {
+  constructor(config) {
+    // set up the image
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    };
+
+    // shadow
+    this.shadow = new Image();
+    this.useShadow = true; // config.useShadow || false
+    if (this.useShadow) {
+      this.shadow.src = "/images/characters/shadow.png";
+    }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    };
+
+    // configure animation and initial state
+    this.animations = config.animations || {
+      idleDown: [[0, 0]],
+    };
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
+
+    // reference the game object
+    this.gameObject = config.gameObject;
+  }
+
+  draw(ctx) {
+    const x = this.gameObject.x * 32;
+    const y = this.gameObject.y * 32;
+
+    this.isShadowLoaded && ctx.drawImage(this.shadow, x, y + 7);
+
+    this.isLoaded && ctx.drawImage(this.image, 0, 0, 66, 66, x, y, 66, 66);
+  }
+}
